beb182051d5d56d81178d7cc7e1f0c11a40e7e9d
wyt1234 <976582772@qq.com>
2022-05-04 01:41:17 +0800

    半夜解决一些问题，扩展了一个版本

--- step7_wastage.py
+++ step7_wastage.py
@@ -9,6 +9,9 @@ from tqdm import tqdm
 import warnings
 import os
 import threading
+
+from typing import List
+
 from utils.info_util import info
 
 # 忽略警告
@@ -114,7 +117,7 @@ def training(df: pandas.DataFrame):
 # 事假
 def affairs(df: pandas.DataFrame):
     info('正在计算-事假')
-    df2 = df[df['状态'].str.contains('事假')].groupby(
+    df2 = df[df['状态'].str.contains('事假') | df['状态'].str.contains('丧假')].groupby(
         ['分运营中心', '行方级别', '状态', '当日产能', '当日应计产能']).size().reset_index().rename(
         columns={0: '数量'})
     df2 = append_loss(df2)
@@ -144,9 +147,19 @@ def absenteeism(df: pandas.DataFrame):
     return df2
 
 
+# 统一美化格式 输出两位小数
+def good_format(df2: pandas.DataFrame, cols: List = None):
+    if not cols:
+        cols = ['损耗产能(单量)', '损耗产能(总量)', '损耗金额(总量)', '人天', '人月']
+    for col in cols:
+        if col in df2.columns.tolist():
+            df2[col] = df2[col].astype('float64').round(2).apply(lambda x: '{:.2f}'.format(x))
+    return df2
+
+
 # 总表
 def summary(df_list: list):
-    state_list = ['病假', '产假', '待岗', '调休', '婚假', '年假', '培训期', '事假', '离职']
+    state_list = ['病假', '产假', '待岗', '调休', '婚假', '年假', '培训期', '事假', '离职', '旷工']
     temp_df_list = []
     for df in df_list:
         temp_df = df.loc['Total'][['损耗产能(总量)', '数量', '损耗金额(总量)']].to_frame().T
@@ -157,6 +170,23 @@ def summary(df_list: list):
         temp_df_list.append(temp_df)
     df_summary = pd.concat(temp_df_list)
     return df_summary
+
+
+# 各个分运营中心横向统计
+def transverse(df_list: List[pandas.DataFrame], center_name_list: List):
+    dic_list = []
+    state_list = ['病假', '产假', '待岗', '调休', '婚假', '年假', '培训期', '事假', '离职', '旷工']
+    for i, center in enumerate(center_name_list):
+        d = {}
+        d['服务提供者部门'] = center
+        for j, state in enumerate(state_list):
+            df2 = df_list[j]
+            df3_amt_sum = df2[df2['分运营中心'] == center]['损耗产能(总量)'].sum()
+            d[state] = df3_amt_sum
+        dic_list.append(d)
+    df_transverse = pd.DataFrame(dic_list)
+    df_transverse = good_format(df_transverse, cols=state_list)
+    return df_transverse
 
 
 def run(wastage_from=None, save_path=None):
@@ -187,23 +217,28 @@ def run(wastage_from=None, save_path=None):
     df_affairs = affairs(df)
     df_dimission = dimission(df)
     df_absenteeism = absenteeism(df)
-    #
-    df_summary = summary(
-        [df_sick, df_maternity, df_waiting, df_rest, df_marriage, df_annual, df_training, df_affairs, df_dimission,
-         df_absenteeism])
+    # 总表计算 -> 注意这里有顺序要求
+    df_list = [df_sick, df_maternity, df_waiting, df_rest, df_marriage, df_annual, df_training, df_affairs,
+               df_dimission, df_absenteeism]
+    df_summary = summary(df_list)
+    # 各运营中心横向统计
+    center_name_list = df['分运营中心'].unique().tolist()
+    center_name_list.sort()
+    df_transverse = transverse(df_list, center_name_list)
     # 导出到一张excel
-    with pd.ExcelWriter(os.path.join(save_path, '产能损耗统计表.xlsx')) as writer:
-        df_summary.to_excel(writer, '总表', index=False)
-        df_sick.to_excel(writer, '病假', index=False)
-        df_maternity.to_excel(writer, '产假', index=False)
-        df_waiting.to_excel(writer, '待岗', index=False)
-        df_rest.to_excel(writer, '调休', index=False)
-        df_marriage.to_excel(writer, '婚假', index=False)
-        df_annual.to_excel(writer, '年假', index=False)
-        df_training.to_excel(writer, '培训期', index=False)
-        df_affairs.to_excel(writer, '事假', index=False)
-        df_dimission.to_excel(writer, '离职', index=False)
-        df_absenteeism.to_excel(writer, '旷工', index=False)
+    with pd.ExcelWriter(os.path.join(save_path, '产能损耗统计表.xlsx'), engine='xlsxwriter') as writer:
+        good_format(df_summary).to_excel(writer, '总表', index=False)
+        good_format(df_transverse).to_excel(writer, '分运营中心', index=False)
+        good_format(df_sick).to_excel(writer, '病假', index=False)
+        good_format(df_maternity).to_excel(writer, '产假', index=False)
+        good_format(df_waiting).to_excel(writer, '待岗', index=False)
+        good_format(df_rest).to_excel(writer, '调休', index=False)
+        good_format(df_marriage).to_excel(writer, '婚假', index=False)
+        good_format(df_annual).to_excel(writer, '年假', index=False)
+        good_format(df_training).to_excel(writer, '培训期', index=False)
+        good_format(df_affairs).to_excel(writer, '事假', index=False)
+        good_format(df_dimission).to_excel(writer, '离职', index=False)
+        good_format(df_absenteeism).to_excel(writer, '旷工', index=False)
         writer.save()
     info('产能损失导出完成')
 
--- step8_wastage_by_state.py
+++ step8_wastage_by_state.py
@@ -1,4 +1,5 @@
 import datetime
+from typing import List
 
 import pandas
 import pandas as pd
@@ -35,6 +36,16 @@ def total_by_df(df):
     return df
 
 
+# 统一美化格式 输出两位小数
+def good_format(df2: pandas.DataFrame, cols: List = None):
+    if not cols:
+        cols = ['损耗产能(单量)', '损耗产能(总量)', '损耗金额(总量)', '人天', '人月']
+    for col in cols:
+        if col in df2.columns.tolist():
+            df2[col] = df2[col].astype('float64').round(2).apply(lambda x: '{:.2f}'.format(x))
+    return df2
+
+
 # 总表
 def summary(df_list: list, state_list: list):
     temp_df_list = []
@@ -42,14 +53,27 @@ def summary(df_list: list, state_list: list):
         temp_df = df.loc['Total'][['损耗产能(总量)', '数量', '损耗金额(总量)']].to_frame().T
         temp_df = temp_df.reset_index(drop=True)
         temp_df['人天'] = temp_df['损耗产能(总量)'] / temp_df['数量'] * (300000 / 264)
-        temp_df['人天'] = temp_df['人天'].astype('float32').round(2)
         temp_df['人月'] = temp_df['人天'] / 22
-        temp_df['人月'] = temp_df['人月'].astype('float32').round(2)
         temp_df['状态'] = state_list[i]
-        temp_df[['损耗产能(总量)', '损耗金额(总量)']] = temp_df[['损耗产能(总量)', '损耗金额(总量)']].astype('float32').round(2)
         temp_df_list.append(temp_df)
     df_summary = pd.concat(temp_df_list)
     return df_summary
+
+
+# 各个分运营中心横向统计
+def transverse(df_list: List[pandas.DataFrame], center_name_list: List, state_list: List):
+    dic_list = []
+    for i, center in enumerate(center_name_list):
+        d = {}
+        d['服务提供者部门'] = center
+        for j, state in enumerate(state_list):
+            df2 = df_list[j]
+            df3_amt_sum = df2[df2['分运营中心'] == center]['损耗产能(总量)'].sum()
+            d[state] = df3_amt_sum
+        dic_list.append(d)
+    df_transverse = pd.DataFrame(dic_list)
+    df_transverse = good_format(df_transverse, state_list)
+    return df_transverse
 
 
 def run(wastage_from=None, save_path=None):
@@ -89,12 +113,19 @@ def run(wastage_from=None, save_path=None):
             df_temp = append_loss(df_temp)
             df_temp = total_by_df(df_temp)
             df_list.append(df_temp)
-        # 导出到一张excel
+        # 总表
         df_summary = summary(df_list, state_list)
-        with pd.ExcelWriter(os.path.join(save_path, f'产能损耗统计表{y_m}.xlsx')) as writer:
-            df_summary.to_excel(writer, '总表', index=False)
+        # 各运营中心横向统计
+        center_name_list = df['分运营中心'].unique().tolist()
+        center_name_list.sort()
+        df_transverse = transverse(df_list, center_name_list, state_list)
+        # 导出到一张excel
+        xlsx_path = Path(os.path.join(save_path, f'产能损耗统计表{y_m}.xlsx'))
+        with pd.ExcelWriter(xlsx_path, engine='xlsxwriter') as writer:
+            good_format(df_summary).to_excel(writer, '总表', index=False)
+            good_format(df_transverse).to_excel(writer, '分运营中心', index=False)
             for df2 in df_list:
-                df2.to_excel(writer, df2['状态'][0], index=False)
+                good_format(df2).to_excel(writer, df2['状态'][0], index=False)
             writer.save()
 
     info('产能损失导出完成')
